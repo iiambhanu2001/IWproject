8234adba994af0b4563b55d3039d78507d0c2b89
Bhanu Pratap <pratap.b2001@gmail.com>
2026-08-23 01:16:42 +0530

    corserror

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://i-wproject.vercel.app/",
     credentials: "true",
   },
 });
