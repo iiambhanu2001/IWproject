18abc0644d60c6aaa977b070ebf98c4d6da5f0a8
Bhanu Pratap <pratap.b2001@gmail.com>
2026-08-29 00:35:35 +0530

    same site

--- backend/Auth/Authcontrollers/authcontrollers.js
+++ backend/Auth/Authcontrollers/authcontrollers.js
@@ -63,7 +63,7 @@ const login = async (req, res) => {
 
             res.cookie('token', token, {
                 httpOnly: true,
-                samesite: "none",
+                sameSite: "none",
                 secure: true
 
             })
